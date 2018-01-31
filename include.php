8213cec7beba8e5fa6e5f645e484a8e5a91146d6
chenyaco <35962393+chenyaco@users.noreply.github.com>
2018-01-31 20:36:52 +0800

    updata

---
+++ include.php
@@ -0,0 +1,325 @@
+<header>
+<!-- As a link -->
+		<div class="bg-loch d-none d-md-block py-3">
+			<div class="text-center">
+				<a class="text-white">Free shipping on all orders over $50</a>
+			</div>
+		</div>
+		<!-- navbar -->
+		<div class="bg-light navbg-light sticky-top">
+			<div class="container py-1">
+				<nav class="navbar navbar-expand-md navbar-light pl-0">
+					<h1 class="navbar-brand logo" href="#">
+						<a href="">seaside</a>
+					</h1>
+
+					<div class="ml-auto">
+						<a class="d-md-none d-inline-flex text-secondary" data-toggle="modal" data-target="#search" href="#">
+							<i class="fa fa-search" aria-hidden="true"></i>
+						</a>
+						<div class=" d-md-none d-inline-flex dropdown">
+							<!-- 購物車 -->
+							<a class="nav-link btn btn-sm text-left text-secondary" data-toggle="dropdown" data-flip="false">
+								<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+								<span class="badge badge-pill badge-danger">3</span>
+							</a>
+							<div class="dropdown-menu dropdown-menu-right" style="min-width:300px;">
+								<div class="px-4 py-3">
+									<h6>已選擇商品</h6>
+									<table class="table table-sm">
+										<tbody>
+											<tr>
+												<td class="align-middle text-center">
+													<a href="#removeModal" class="text-muted" data-toggle="modal" data-target="#trashModel" data-title="刪除 療癒暖暖黃杯 件">
+														<i class="fa fa-trash-o" aria-hidden="true"></i>
+													</a>
+												</td>
+												<td class="align-middle">療癒暖暖黃杯</td>
+												<td class="align-middle">1件</td>
+												<td class="align-middle text-right">$420</td>
+											</tr>
+											<tr>
+												<td class="align-middle text-center">
+													<a href="#removeModal" class="text-muted" data-toggle="modal" data-target="#trashModel" data-title="刪除 極簡灰調茶杯組 1 件">
+														<i class="fa fa-trash-o" aria-hidden="true"></i>
+													</a>
+												</td>
+												<td class="align-middle">極簡灰調茶杯組</td>
+												<td class="align-middle">1件</td>
+												<td class="align-middle text-right">$380</td>
+											</tr>
+											<tr>
+												<td class="align-middle text-center">
+													<a href="#removeModal" class="text-muted" data-toggle="modal" data-target="#trashModel" data-title="刪除 療癒暖暖綠杯 1 件">
+														<i class="fa fa-trash-o" aria-hidden="true"></i>
+													</a>
+												</td>
+												<td class="align-middle">療癒暖暖綠杯</td>
+												<td class="align-middle">1件</td>
+												<td class="align-middle text-right">$520</td>
+											</tr>
+										</tbody>
+									</table>
+									<a href="" class="btn btn-primary btn-block">結帳去</a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+					 aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarNavDropdown">
+						<ul class="navbar-nav ml-auto">
+							<li class="nav-item active text-center">
+								<a class="nav-link" href="#">首頁
+									<span class="sr-only">(current)</span>
+								</a>
+							</li>
+							<li class="nav-item dropdown text-center">
+								<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									商店
+								</a>
+								<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+									<a class="dropdown-item" href="#">全部商品</a>
+									<a class="dropdown-item" href="#">茶杯</a>
+									<a class="dropdown-item" href="#">茶葉</a>
+								</div>
+							</li>
+							<li class="nav-item text-center">
+								<a class="nav-link" href="#">關於我們</a>
+							</li>
+							<li class="nav-item text-center">
+								<a class="nav-link" data-toggle="modal" data-target="#loginModel" href="#">登入</a>
+							</li>
+							<li class="nav-item text-center">
+								<a class="nav-link" data-toggle="modal" data-target="#registerModel" href="#">註冊</a>
+							</li>
+							<li class="nav-item text-center  d-none d-md-block">
+								<a class="nav-link" data-toggle="modal" data-target="#search" href="#">
+									<i class="fa fa-search" aria-hidden="true"></i>
+								</a>
+							</li>
+							<li class="nav-item text-center d-none d-md-block">
+								<!-- <a class="nav-link" href="#">
+									<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+									<span class="badge badge-pill badge-secondary">3</span>
+								</a> -->
+								<div class="dropdown">
+									<!-- 購物車 -->
+									<a class="nav-link btn btn-sm text-left" data-toggle="dropdown" data-flip="false">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										<span class="badge badge-pill badge-danger">3</span>
+									</a>
+									<div class="dropdown-menu dropdown-menu-right" style="min-width:300px;">
+										<div class="px-4 py-3">
+											<h6>已選擇商品</h6>
+											<table class="table table-sm">
+												<tbody>
+													<tr>
+														<td class="align-middle text-center">
+															<a href="#removeModal" class="text-muted" data-toggle="modal" data-target="#trashModel" data-title="刪除 療癒暖暖黃杯 件">
+																<i class="fa fa-trash-o" aria-hidden="true"></i>
+															</a>
+														</td>
+														<td class="align-middle">療癒暖暖黃杯</td>
+														<td class="align-middle">1件</td>
+														<td class="align-middle text-right">$420</td>
+													</tr>
+													<tr>
+														<td class="align-middle text-center">
+															<a href="#removeModal" class="text-muted" data-toggle="modal" data-target="#trashModel" data-title="刪除 極簡灰調茶杯組 1 件">
+																<i class="fa fa-trash-o" aria-hidden="true"></i>
+															</a>
+														</td>
+														<td class="align-middle">極簡灰調茶杯組</td>
+														<td class="align-middle">1件</td>
+														<td class="align-middle text-right">$380</td>
+													</tr>
+													<tr>
+														<td class="align-middle text-center">
+															<a href="#removeModal" class="text-muted" data-toggle="modal" data-target="#trashModel" data-title="刪除 療癒暖暖綠杯 1 件">
+																<i class="fa fa-trash-o" aria-hidden="true"></i>
+															</a>
+														</td>
+														<td class="align-middle">療癒暖暖綠杯</td>
+														<td class="align-middle">1件</td>
+														<td class="align-middle text-right">$520</td>
+													</tr>
+												</tbody>
+											</table>
+											<a href="" class="btn btn-primary btn-block">結帳去</a>
+										</div>
+									</div>
+								</div>
+								<!-- 購物車END -->
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
+		<!-- navbar END -->
+
+
+	<!-- loginModel -->
+	<div class="modal fade" id="loginModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header bg-loch">
+					<h5 class="modal-title text-white" id="exampleModalLabel">登入</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<form>
+						<div class="form-group">
+							<label for="exampleInputEmail1">Email</label>
+							<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="請輸入email">
+							<small id="emailHelp" class="form-text text-muted">我們絕不會隨意將你的 email 給予第三者</small>
+						</div>
+						<div class="form-group">
+							<label for="exampleInputPassword1">密碼</label>
+							<input type="password" class="form-control" id="exampleInputPassword1" placeholder="請輸入密碼">
+						</div>
+						<div class="form-check">
+							<label class="form-check-label">
+								<input type="checkbox" class="form-check-input"> 記住帳號
+							</label>
+						</div>
+					</form>
+				</div>
+				<div class="modal-footer">
+					<a href="" class="mr-auto" data-dismiss="modal" data-toggle="modal" data-target="#registerModel">切換到註冊</a>
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+					<button type="button" class="btn btn-loch text-white">登入</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+	<!-- registerModel -->
+	<div class="modal fade" id="registerModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+				<div class="modal-header bg-loch">
+					<h5 class="modal-title text-white" id="exampleModalLabel">註冊seaside</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="row">
+						<div class="col">
+							<h3>seaside</h3>
+							<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae quisquam ipsa dolore, officiis maiores impedit
+								laboriosam tempore earum eos minima!</p>
+							<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae quisquam ipsa dolore, officiis maiores impedit
+								laboriosam tempore earum eos minima!</p>
+
+						</div>
+						<div class="col">
+							<form>
+								<div class="form-group">
+									<label for="exampleInputEmail1">Email</label>
+									<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="請輸入email">
+									<small id="emailHelp" class="form-text text-muted">我們絕不會隨意將你的 email 給予第三者</small>
+								</div>
+								<div class="form-group">
+									<label for="exampleInputPassword1">密碼</label>
+									<input type="password" class="form-control" id="exampleInputPassword1" placeholder="請輸入密碼">
+								</div>
+								<div class="form-group">
+									<label for="exampleInputPassword1">確認密碼</label>
+									<input type="password" class="form-control" id="exampleInputPassword1" placeholder="請確認密碼">
+								</div>
+								<div class="form-check">
+									<label class="form-check-label">
+										<input type="checkbox" class="form-check-input"> 同意條款
+									</label>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<a href="" class="mr-auto" data-dismiss="modal" data-toggle="modal" data-target="#loginModel">切換到登入</a>
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+					<button type="button" class="btn btn-loch text-white">註冊</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+	<!-- loginModel -->
+	<div class="modal fade" id="search" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header bg-loch">
+					<h5 class="modal-title text-white" id="exampleModalLabel">查詢</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="input-group mb-3">
+						<input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2">
+						<div class="input-group-append">
+							<button class="btn btn-outline-loch" type="button">查詢</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+	<!-- trash Model -->
+	<div class="modal" id="trashModel" tabindex="-1" role="dialog">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header bg-danger">
+					<h5 class="modal-title text-white">刪除商品</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<p>是否確認刪除商品</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-outline-secondary">否</button>
+					<button type="button" class="btn btn-outline-danger px-5" data-dismiss="modal">是</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!--  trash Model END  -->
+
+</header>	
+
+
+
+
+
+
+
+
+
+
+
+		
